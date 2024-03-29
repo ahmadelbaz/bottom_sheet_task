6c04c0dffc926567330c5f96529001a10c15bc8d
ahmadelbaz <ahmeed.elbaz@gmail.com>
2024-03-29 03:42:05 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/bottom_sheet_task_ahmad_elbaz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bottom_sheet_task_ahmad_elbaz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bottom_sheet_task_ahmad_elbaz/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
