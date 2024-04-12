6f6d81514df905c3609764fef381858644fac3b0
ahmadelbaz <ahmeed.elbaz@gmail.com>
2024-04-12 06:39:19 +0200

    Added UI component in the bottomsheet dragger , Add s6 constant and update CustomBottomSheet dragHandlerColor default value
    Added a new constant s6 in value_manager.dart and updated the default value for dragHandlerColor in CustomBottomSheet to ColorManager.grey1.

--- lib/widgets/custom_bottom_sheet.dart
+++ lib/widgets/custom_bottom_sheet.dart
@@ -1,13 +1,16 @@
+import 'package:bottom_sheet_task_ahmad_elbaz/resources/color_manager.dart';
 import 'package:bottom_sheet_task_ahmad_elbaz/resources/value_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CustomBottomSheet extends ConsumerWidget {
   final Widget child;
-  final Color? dragHandlerColor;
-  const CustomBottomSheet(
-      {required this.child, this.dragHandlerColor, super.key});
-
+  final Color dragHandlerColor;
+  const CustomBottomSheet({
+    required this.child,
+    Color? dragHandlerColor,
+    super.key,
+  }) : dragHandlerColor = dragHandlerColor ?? ColorManager.grey1;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Container(
@@ -30,7 +33,19 @@ class CustomBottomSheet extends ConsumerWidget {
             right: 0.0,
             height: AppSizes.s30, // Adjust grab bar height as needed
             child: Container(
-              color: dragHandlerColor!.withOpacity(0.8),
+              color: dragHandlerColor.withOpacity(0.8),
+              child: Center(
+                child: Container(
+                  width: AppSizes.s50,
+                  height: AppSizes.s6,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(AppSizes.s10),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
         ],
